c913a4a56118f8d3d8524da5486f8d1e26fd7c88
Flzzyf <flzzyf076@gmail.com>
2018-07-17 21:42:53 +0800

    音量修正

--- FuryBrick/Assets/Script/Util/Sound/Sound.cs
+++ FuryBrick/Assets/Script/Util/Sound/Sound.cs
@@ -8,7 +8,7 @@ public class Sound
     public string name;
     public AudioClip clip;
 
-    [Range(0f, 3f)]
+    [Range(0f, 1f)]
     public float volume = 1;
     [Range(.1f, 3f)]
     public float pitch = 1;
